6101b65ca20f8580809e837c2e59a897ff42acb4
Ekaterina Kadyrova <pin-ka@outlook.com>
2019-02-12 23:46:37 +0500

    Изменен логотип

--- desktop/src/ru/pin_ka/desktop/DesktopLauncher.java
+++ desktop/src/ru/pin_ka/desktop/DesktopLauncher.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import ru.pin_ka.Sweet2DGame;
-import ru.pin_ka.base.Base2DScreen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
